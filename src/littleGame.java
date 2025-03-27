3e5a93ac83c22f6105794721e846cc0c76ae9333
Nadia <109280529+nadiavali@users.noreply.github.com>
2025-03-27 10:25:43 +0100

    update littleGame

--- src/littleGame.java
+++ src/littleGame.java
@@ -1,12 +1,22 @@
  public class littleGame {
     public static void main(String[] args) {
 
-        finalScoreCalculator(true, 100, 800, 5);
-        finalScoreCalculator(true, 200, 10000, 8);
+       
+        int lowScore = littleGame.finalScoreCalculator(true, 9, 8, 7);
+        int highScore =  finalScoreCalculator(true, 200, 10000, 8);
+        System.out.println(lowScore);
+        System.out.println(highScore);
      
     }
    
-    public static void finalScoreCalculator(boolean gameOver,int bonus,int score,int levelCompleted) {
+    /**
+     * @param gameOver
+     * @param bonus
+     * @param score
+     * @param levelCompleted
+     * @return
+     */
+    public static int finalScoreCalculator(boolean gameOver,int bonus,int score,int levelCompleted) {
         
         int finalScore = score;
 
@@ -14,8 +24,10 @@
             
             finalScore += (levelCompleted * bonus);
             finalScore += 1000;
-            System.out.println("Youre final score was: " + finalScore);
         }
-    
+        
+        return finalScore;
     }
  }
+
+ 
