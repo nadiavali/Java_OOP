8ed3bbf8ccecdaf5083d573950c210810cdc7a6f
Nadia <109280529+nadiavali@users.noreply.github.com>
2025-03-29 10:11:52 +0100

    add leap year calc

---
+++ src/LeapYear.java
@@ -0,0 +1,7 @@
+public class LeapYear {
+    // write code here
+    public static boolean isLeapYear(int year){
+       return year >= 1 && year <= 9999 && year % 4 == 0 && (year % 100 != 0 || year % 400 == 0);
+        
+    }
+}
