2ffab69c53ad1fc4ef147fbbaa32a2a9fe0cca1b
Nadia <109280529+nadiavali@users.noreply.github.com>
2025-04-05 10:40:06 +0200

    modify  Areacalculater

--- src/AreaCalculator.java
+++ src/AreaCalculator.java
@@ -1,19 +1,19 @@
-public class SeconMinuetes {
+// public class AreaCalculator {
 
-    public static void main(String[] args){
-        System.out.println(getDurationString(3540));
-        System.out.println(getDurationString(40,34));
+//     public static void main(String[] args){
+//         System.out.println(getDurationString(3540));
+//         System.out.println(getDurationString(40,34));
 
-    }
-    public static String getDurationString(int seconds){
+//     }
+//     public static String getDurationString(int seconds){
 
-        return getDurationString(seconds/ 60, seconds% 60);
-    }
-    public static String getDurationString(int minutes, int seconds){
-        //return seconds >= 59 ? (seconds / 3600+ "h " + ((seconds % 3600) / 60 + ) + "m " + seconds + "s" ): "Please enter the second in range 0 and 60";
-        return seconds <= 59 ? minutes / 60 + "h " + minutes % 60 + "m " + seconds + "s ": "Please enter the second in range 0 and 590";
-    }
-}
+//         return getDurationString(seconds/ 60, seconds% 60);
+//     }
+//     public static String getDurationString(int minutes, int seconds){
+//         //return seconds >= 59 ? (seconds / 3600+ "h " + ((seconds % 3600) / 60 + ) + "m " + seconds + "s" ): "Please enter the second in range 0 and 60";
+//         return seconds <= 59 ? minutes / 60 + "h " + minutes % 60 + "m " + seconds + "s ": "Please enter the second in range 0 and 590";
+//     }
+// }
 
 
 public class AreaCalculator {
