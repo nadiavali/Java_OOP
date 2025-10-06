90fd35b9addc4b72861d0d84b6ea7d3106d50e10
Nadia <109280529+nadiavali@users.noreply.github.com>
2025-10-06 10:28:41 +0200

    delete doublication and add extra days to day of the week

--- src/WeekDay.java
+++ src/WeekDay.java
@@ -6,6 +6,11 @@ public class WeekDay {
         switch (day) {
             case 0 -> System.out.println("Sunday");
             case 1 -> System.out.println("Monday");
+            case 2 -> System.out.println("Tuesday");
+            case 3 -> System.out.println("Wedensday");
+            case 4 -> System.out.println("Thursday");
+            case 5 -> System.out.println("Friday");
+            case 6 -> System.out.println("Saturday");
             default -> System.out.println("Invalid Day");
 
     }
