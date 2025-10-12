ab26ee29e7cf896672c2d962e995bc92b6d3cd92
Nadia <109280529+nadiavali@users.noreply.github.com>
2025-10-12 14:54:11 +0200

    add sum of even digits

---
+++ src/EvenDigitSum.java
@@ -0,0 +1,26 @@
+public class EvenDigitSum{
+    public static void main(String[] args){
+        System.out.println(getEvenDigitSum(38968));
+    }
+    public static int getEvenDigitSum(int number){
+        if (number < 0){
+            return -1;
+        }
+        int sum = 0;
+      
+        while (number > 0){
+            int digit = number % 10;
+            number = number / 10;
+   
+            if (digit % 2 == 0){
+              
+                sum += digit;
+            }
+        } 
+        if (number % 2==0){
+            sum += number;
+            
+        }
+        return sum;
+    }
+}
