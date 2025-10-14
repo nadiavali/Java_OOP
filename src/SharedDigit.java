7480bb673915266515d988ebefb21f0949a43da6
Nadia <109280529+nadiavali@users.noreply.github.com>
2025-10-14 11:42:16 +0200

    modify the shared digit, shortening the first version

--- src/SharedDigit.java
+++ src/SharedDigit.java
@@ -1,16 +1,18 @@
 public class SharedDigit {
     public static void main(String[] args){
-        System.out.println(hasSharedDigit(100,10 ));
+        System.out.println(hasSharedDigit(24,34 ));
     
     }
     public static boolean  hasSharedDigit(int number1, int number2){
-        if(10 <= number1 && 10 <= number2 && number2<=99 && number1 <=99){
-           if (number1 % 10 == number2 % 10 || number1 % 10 == number2 / 10 || number1 / 10 == number2 % 10 || number1 / 10 == number2 / 10){
-            return true;
-           }else{
+        if (number1 < 10 || number2 < 10 || number2 > 99 || number1 > 99) {
             return false;
-           }
         }
-        return false;
+        int n1 = number1 / 10, n2 = number1 % 10;
+        int n3 = number2 / 10, n4 = number2 % 10;
+        
+          
+        return (n1 == n3 || n1 == n4 || n2 == n3 || n2 == n4);
+           
     }
 }
+
