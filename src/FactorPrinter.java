6d325db5f41032347c184e9d4878018778d183bc
Nadia <109280529+nadiavali@users.noreply.github.com>
2025-10-14 12:09:10 +0200

    print all factors of a number

---
+++ src/FactorPrinter.java
@@ -0,0 +1,19 @@
+public class FactorPrinter{
+    public static void main(String[] args){
+        printFactors(32);
+
+    }
+    public static void printFactors(int number){
+        if (number < 1){
+            System.out.println("Invalid Value");
+        }
+        int n = 1;
+    
+        while (n <= number){
+            if (number % n == 0){
+                System.out.println(n);
+            }
+            n++;
+        }
+    }
+}
