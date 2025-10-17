951e0fb704fb94e29d18e84dfec752bce051ca43
Nadia <109280529+nadiavali@users.noreply.github.com>
2025-10-17 09:40:27 +0200

    add interesting Diagonal stars to my Java pack

---
+++ src/DiagonalStar.java
@@ -0,0 +1,25 @@
+public class DiagonalStar {
+
+    public static void main(String[] args){
+      printSquareStar(5);
+    }
+
+    public static void printSquareStar(int number){
+        if (number < 5) {
+            System.out.println("Invalid Value");
+        }
+
+        int row  = number;
+        int col = number;
+        for (row = number; row >= 1; row --){  // row 5, col 5,4,3,2,1--- row 4 col 5,4,3,2,1.....
+            for(col = number; col >= 1; col --){
+                if (row == 1 || col == 1 || row == number || col == number || row == col || col == number - row + 1) {
+                    System.out.print("*");                
+                }else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println(); // after each row is completed
+        }
+    }
+}
