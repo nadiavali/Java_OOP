b43b9a242358d5cabd0ccffe6bb36f6c89d7c8f8
Nadia <109280529+nadiavali@users.noreply.github.com>
2025-10-31 11:55:13 +0100

    add BedroomCompo.. showcase composition

---
+++ src/BedroomComposition/Ceiling.java
@@ -0,0 +1,26 @@
+package BedroomComposition;
+
+public class Ceiling {
+    private int height;
+    private int paintedColor;
+
+
+    public Ceiling(int height, int paintedColor) {
+        this.height = height;
+        this.paintedColor = paintedColor;
+    }
+
+
+    public int getHeight() {
+        return height;
+    }
+
+
+    public int getPaintedColor() {
+        return paintedColor;
+    }
+
+    
+
+    
+}
---
+++ src/BedroomComposition/Wall.java
@@ -0,0 +1,19 @@
+package BedroomComposition;
+
+public class Wall {
+    private String direction;
+
+    
+
+    public Wall(String direction) {
+        this.direction = direction;
+    }
+
+
+
+    public String getDirection() {
+        return direction;
+    }
+
+    
+}
