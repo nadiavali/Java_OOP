c6d7d9ed4ed55c84fd9679e5866591f78dedcfb9
Nadia <109280529+nadiavali@users.noreply.github.com>
2025-11-03 12:35:58 +0100

    add movie as showcase of polymorphism

---
+++ src/Polymorphism/Movie.java
@@ -0,0 +1,82 @@
+package Polymorphism;
+
+public class Movie {
+
+    private String title;
+
+    public Movie() {}
+    public Movie(String title) {
+        this.title = title;
+    }
+
+    public void watchMovie(){
+        String instanceType = this.getClass().getSimpleName();
+        System.out.println(title + " is a " + instanceType + " film.");
+    }
+
+    public static Movie getMovie(String type, String title) {
+        return switch(type.toUpperCase().charAt(0)) {
+            case 'A' -> new Adventure(title);
+            case 'C' -> new Comedy(title);
+            case 'S' -> new SienceFiction(title);
+            
+            default -> new Movie(title);          
+            
+        };
+    }
+}
+
+
+class Adventure extends Movie {
+
+    public Adventure(String title) {
+        super(title);
+    }
+
+    @Override
+    public void watchMovie() {
+        super.watchMovie();
+        System.out.printf("...%s %n".repeat(3),
+        "Pleseant Scence",
+        "Scary Music",
+        "Something bad happens");
+    }
+    
+}
+
+
+
+class Comedy extends Movie {
+
+    public Comedy(String title) {
+        super(title);
+    }
+
+    @Override
+    public void watchMovie() {
+        super.watchMovie();
+        System.out.printf("...%s %n".repeat(3),
+        "Something funny happend",
+        "Funny Music",
+        "happy ending...");
+    }
+    
+}
+
+
+class SienceFiction extends Movie {
+
+    public SienceFiction(String title) {
+        super(title);
+    }
+
+    @Override
+    public void watchMovie() {
+        super.watchMovie();
+        System.out.printf("...%s %n".repeat(3),
+        "Aliens are fighting",
+        "Alien Music",
+        "Humen won the war...");
+    }
+    
+}
