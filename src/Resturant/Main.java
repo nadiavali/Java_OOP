3c6c8482ae64a8d46bc810c7056b78161ceba9d2
Nadia <109280529+nadiavali@users.noreply.github.com>
2025-11-05 09:04:51 +0100

    add resturant showcase class, composition encapsulation, poly...

--- src/Resturant/Main.java
+++ src/Resturant/Main.java
@@ -3,7 +3,29 @@ package Resturant;
 public class Main {
 
     public static void main(String[] args) {
-        Drink drink = new Drink("Coke", "Medium", 10);
-        drink.drinkPrice();
+    
+        // Item coke = new Item("Coke", "Drink", 5);
+        // Item avocado = new Item("Avocado", "Topping", 2);
+
+        // avocado.printItem();
+        // avocado.setSize("LARGE");
+        // avocado.getBasePrice();
+        // avocado.printItem();
+
+        // Burger burger = new Burger("Ham", 3);
+        // burger.addedTopping("HAM","AVOCADO", "MAyo");
+        // burger.printItem();
+        // Meal regularMeal = new Meal();
+        // regularMeal.addBurgerToppings("Mayo", "Ham", "Cheese");
+        // regularMeal.setDrinkSize("Large");
+        // regularMeal.printItemizedList();
+
+
+        Meal meal = new Meal("Turkey", "7-up", "Chili");
+        meal.addBurgerToppings("ham", "Cheese", "Tomatoes");
+        meal.setDrinkSize("SMALL");
+        meal.printItemizedList();
     }
+
+
 }
