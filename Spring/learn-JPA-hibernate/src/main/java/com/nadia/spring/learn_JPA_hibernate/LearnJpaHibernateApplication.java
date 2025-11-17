448d6752cc5eacd5ba7d2d30fc616bce10c67944
Nadia <109280529+nadiavali@users.noreply.github.com>
2025-11-17 10:51:14 +0100

    add project and config with create table in sql and connetion

---
+++ Spring/learn-JPA-hibernate/src/main/java/com/nadia/spring/learn_JPA_hibernate/LearnJpaHibernateApplication.java
@@ -0,0 +1,13 @@
+package com.nadia.spring.learn_JPA_hibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnJpaHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnJpaHibernateApplication.class, args);
+	}
+
+}
