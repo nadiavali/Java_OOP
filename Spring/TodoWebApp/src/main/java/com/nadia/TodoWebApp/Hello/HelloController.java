77b60286809e9556f95f15836eebc0ff3a7c9060
Nadia <109280529+nadiavali@users.noreply.github.com>
2025-11-18 15:34:03 +0100

    add new proj web app todo  using html

---
+++ Spring/TodoWebApp/src/main/java/com/nadia/TodoWebApp/Hello/HelloController.java
@@ -0,0 +1,31 @@
+package com.nadia.TodoWebApp.Hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+// Handles requests to /say-hello and returns a plain text message
+// This controller maps the /say-hello URL to a method that returns a greeting string
+// When the user goes to /say-hello in the browser, this method runs and returns a text response
+
+@Controller
+public class HelloController {
+
+    @RequestMapping("say-hello")
+    // @ResponseBody makes all controller methods return data directly in the HTTP response
+    // instead of trying to render a view template
+    // return the string as is
+    @ResponseBody 
+    public String SayHello() {
+        return "HI, What you wanna learn now?";
+    }
+
+
+    @GetMapping("hello")
+    public String HelloHTML() {
+      return "hello";
+    }
+}
+
+
