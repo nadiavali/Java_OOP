cfe2ea865bd8b2bc972cd9fdd5d4ec031fe107b6
Nadia <109280529+nadiavali@users.noreply.github.com>
2025-11-18 17:13:27 +0100

    add logger

--- Spring/TodoWebApp/src/main/java/com/nadia/TodoWebApp/Login/LoginController.java
+++ Spring/TodoWebApp/src/main/java/com/nadia/TodoWebApp/Login/LoginController.java
@@ -1,5 +1,8 @@
 package com.nadia.TodoWebApp.Login;
 
+
+
+import org.slf4j.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,11 +11,14 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class LoginController {
 
+    private Logger logger = LoggerFactory.getLogger(getClass());
 
     @RequestMapping("login")
     public String login(@RequestParam String name, ModelMap model) {
         model.put("name", name); // key in jsp: ${key}
-        System.out.println("Request param is " + name);
+        logger.info("I need this be printed at info level...");
+        logger.warn("I wanna warn sth at warn level");
+        logger.debug("Request param is " + name); // you can turn it off by property
       return "login";
     }
 }
