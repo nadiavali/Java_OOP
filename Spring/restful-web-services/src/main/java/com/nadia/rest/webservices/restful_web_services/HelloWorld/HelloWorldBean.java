60d12e66c87f330986a38b2aac7de512862d3ddc
Nadia <109280529+nadiavali@users.noreply.github.com>
2025-12-02 11:39:39 +0100

    add a simple rest api

---
+++ Spring/restful-web-services/src/main/java/com/nadia/rest/webservices/restful_web_services/HelloWorld/HelloWorldBean.java
@@ -0,0 +1,17 @@
+package com.nadia.rest.webservices.restful_web_services.HelloWorld;
+
+public class HelloWorldBean {
+    private String message;
+
+    public HelloWorldBean(String message) {
+        this.message= message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
