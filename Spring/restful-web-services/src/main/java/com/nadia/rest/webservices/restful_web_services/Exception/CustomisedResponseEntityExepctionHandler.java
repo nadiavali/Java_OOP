55394d547328a9bbf99bf168f95b2651b4269b63
Nadia <109280529+nadiavali@users.noreply.github.com>
2025-12-03 12:22:12 +0100

    customizing my own error handler

---
+++ Spring/restful-web-services/src/main/java/com/nadia/rest/webservices/restful_web_services/Exception/CustomisedResponseEntityExepctionHandler.java
@@ -0,0 +1,36 @@
+package com.nadia.rest.webservices.restful_web_services.Exception;
+
+import java.net.http.HttpRequest;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class CustomisedResponseEntityExepctionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public final ResponseEntity<Object> handleException(Exception ex, WebRequest request) throws Exception{
+        ErrorDetails errorDetails = new ErrorDetails(LocalDate.now(),
+                                     ex.getMessage(), 
+                                    request.getDescription(false));
+        return new ResponseEntity(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+ 
+
+
+	@ExceptionHandler(UserNotFoundException.class)
+	public final ResponseEntity<ErrorDetails> handleUserNotFoundException(Exception ex, WebRequest request) throws Exception {
+        ErrorDetails errorDetails = new ErrorDetails(LocalDate.now(), ex.getMessage(), request.getDescription(false));
+		
+		return new ResponseEntity<ErrorDetails>(errorDetails, HttpStatus.NOT_FOUND);
+		
+	}
+ 
+	
+}
