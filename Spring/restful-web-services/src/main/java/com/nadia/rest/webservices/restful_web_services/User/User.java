0b9b27ff0e0d0ef7e654f176a29543e943768db8
Nadia <109280529+nadiavali@users.noreply.github.com>
2025-12-31 13:10:21 +0100

    add filtering feature

--- Spring/restful-web-services/src/main/java/com/nadia/rest/webservices/restful_web_services/User/User.java
+++ Spring/restful-web-services/src/main/java/com/nadia/rest/webservices/restful_web_services/User/User.java
@@ -2,14 +2,19 @@ package com.nadia.rest.webservices.restful_web_services.User;
 
 import java.time.LocalDate;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 
 public class User {
     private Integer id;
-    @Size(min=2, message="Name should have at least 2 characters")  
+    @Size(min=2, message="Name should have at least 2 characters") 
+    @JsonProperty("user_name") 
     private String name;
+
     @Past(message="Birth date should be in the past")
+    @JsonProperty("birth_date")
     private LocalDate birthDate;
 
 
